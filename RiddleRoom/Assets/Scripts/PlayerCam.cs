d3c6896f7909a4a50ae02777701b307e71e02e6f
Eyzian59 <trunguyen5927@gmail.com>
2024-04-27 16:17:53 -0500

    Camera Updates

--- RiddleRoom/Assets/Scripts/PlayerCam.cs
+++ RiddleRoom/Assets/Scripts/PlayerCam.cs
@@ -4,34 +4,29 @@ using UnityEngine;
 
 public class PlayerCam : MonoBehaviour
 {
-    public float sensX;
-    public float sensY;
+    public float sens = 100f;
 
+    public Transform playerBody;
 
-    public Transform orientation;
-
-    float xRotation;
-    float yRotation;
+    float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sens;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sens;
 
-        yRotation += mouseX;
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
 
     }
 }
